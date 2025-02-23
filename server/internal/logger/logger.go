0a5209b15291b4e8dcee11d8ed162405f3942209
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-02-23 14:49:30 +0300

    feat(server): add logger package

---
+++ server/internal/logger/logger.go
@@ -0,0 +1,27 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+const (
+	envDev  = "dev"
+	envProd = "prod"
+)
+
+// New returns new slog logger for the given env:
+//  - "dev" - text handler with Debug level;
+//  - "prod" - JSON handler with Info level.
+func New(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
