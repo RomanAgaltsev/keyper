ebbf3ad313e7e2ec5a2f03a17227128999928853
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-02-23 17:06:00 +0300

    feat(server): add auth package

--- go.mod
+++ go.mod
@@ -2,11 +2,25 @@ module github.com/RomanAgaltsev/keyper
 
 go 1.23.6
 
-require github.com/ilyakaznacheev/cleanenv v1.5.0
+require (
+	github.com/go-chi/jwtauth/v5 v5.3.2
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/lestrrat-go/jwx/v2 v2.1.3
+	golang.org/x/crypto v0.31.0
+)
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.6 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
---
+++ server/internal/pkg/auth/auth.go
@@ -0,0 +1,58 @@
+// Package auth provides methods for authorization and authentication.
+package auth
+
+import (
+	"time"
+
+	"github.com/RomanAgaltsev/keyper/server/internal/model"
+
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type (
+	UserIDName    string
+	UserLoginName string
+	TokenExpName  string
+)
+
+const (
+	// JWTSignAlgorithm contains JWT signing algorithm.
+	JWTSignAlgorithm = "HS256"
+
+	// UserIDClaimName contains key name of user ID in a context.
+	UserIDClaimName UserIDName = "uid"
+
+	// UserLoginClaimName contains key name of user login in a context.
+	UserLoginClaimName UserLoginName = "login"
+
+	// TokenExpClaimName contains key name of token expiration in a context.
+	TokenExpClaimName TokenExpName = "exp"
+)
+
+// NewAuth returns new JWTAuth.
+func NewAuth(secretKey string) *jwtauth.JWTAuth {
+	return jwtauth.New(JWTSignAlgorithm, []byte(secretKey), nil)
+}
+
+// NewJWTToken creates new JWT token.
+func NewJWTToken(ja *jwtauth.JWTAuth, user model.User, duration time.Duration) (token jwt.Token, tokenString string, err error) {
+	return ja.Encode(map[string]interface{}{
+		string(UserIDClaimName):    user.ID,
+		string(UserLoginClaimName): user.Login,
+		string(TokenExpClaimName):  time.Now().Add(duration).Unix(),
+	})
+}
+
+// HashPassword generates and returns hash of a given password.
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+// CheckPasswordHash compares given password and hash.
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
