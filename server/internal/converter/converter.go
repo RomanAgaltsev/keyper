5c8ecf77bec3166b7f872d1398f63aac6673e36f
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-02-25 11:22:55 +0300

    feat(server): implement gRPC server
    Correct records.proto and regenerate go files.
    
    Add gRPC-server implementation.
    
    Add converter package for types convertion.

---
+++ server/internal/converter/converter.go
@@ -0,0 +1,17 @@
+package converter
+
+import (
+	recordsv1 "github.com/RomanAgaltsev/keyper/pkg/records_v1"
+	"github.com/RomanAgaltsev/keyper/server/internal/model"
+)
+
+func ToRecordsFromServiceRecord(record *model.Record) *recordsv1.Record {
+	//	credencials
+	//
+	//	return &recordsv1.GetResponse{
+	//		Id:      uint64(record.ID),
+	//		Type:    uint32(record.Type),
+	//		Address: record.Address,
+	//		Credentials:
+	return &recordsv1.Record{}
+}
