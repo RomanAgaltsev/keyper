4c2b161b161dfe9cc1de297f7643d80b7a29bfee
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-03-15 17:39:03 +0300

    feat(service): implement user and secret services

---
+++ internal/app/keyper-srv/service/secret.go
@@ -0,0 +1,37 @@
+package service
+
+import (
+	"context"
+
+	"github.com/RomanAgaltsev/keyper/internal/app/keyper-srv/api"
+	"github.com/RomanAgaltsev/keyper/internal/config"
+)
+
+var _ api.SecretService = (*SecretService)(nil)
+
+func NewSecretService(cfg *config.AppConfig) *SecretService {
+	return &SecretService{}
+}
+
+type SecretService struct {
+}
+
+func (s *SecretService) Create(ctx context.Context) error {
+	return nil
+}
+
+func (s *SecretService) Get(ctx context.Context) error {
+	return nil
+}
+
+func (s *SecretService) List(ctx context.Context) error {
+	return nil
+}
+
+func (s *SecretService) Update(ctx context.Context) error {
+	return nil
+}
+
+func (s *SecretService) Delete(ctx context.Context) error {
+	return nil
+}
---
+++ internal/app/keyper-srv/service/user.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"context"
+
+	"github.com/RomanAgaltsev/keyper/internal/app/keyper-srv/api"
+	"github.com/RomanAgaltsev/keyper/internal/config"
+)
+
+var _ api.UserService = (*UserService)(nil)
+
+func NewUserService(cfg *config.AppConfig) *UserService {
+	return &UserService{}
+}
+
+type UserService struct {
+}
+
+func (s *UserService) Register(ctx context.Context) error {
+	return nil
+}
+
+func (s *UserService) Login(ctx context.Context) error {
+	return nil
+}
