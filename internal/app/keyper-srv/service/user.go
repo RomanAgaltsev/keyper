64622ba9bfff23da03ea3825f462961594dc8f49
Roman Agaltsev <roman-agalcev@yandex.ru>
2025-03-21 17:47:41 +0300

    feat(service): implement user service methods

--- internal/app/keyper-srv/service/user.go
+++ internal/app/keyper-srv/service/user.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"log/slog"
 
 	"github.com/cenkalti/backoff/v5"
@@ -10,7 +11,12 @@ import (
 	"github.com/RomanAgaltsev/keyper/internal/model"
 )
 
-var _ UserRepository = (*repository.UserRepository)(nil)
+var (
+	_ UserRepository = (*repository.UserRepository)(nil)
+
+	ErrLoginTaken = errors.New("login has already been taken")
+	ErrLoginWrong = errors.New("wrong login/password")
+)
 
 type UserRepository interface {
 	Create(ctx context.Context, ro []backoff.RetryOption, user model.User) error
@@ -31,9 +37,37 @@ type UserService struct {
 }
 
 func (s *UserService) Register(ctx context.Context, user model.User) error {
+	// TODO: password hashing
+
+	// Create user in the repository
+	err := s.repository.Create(ctx, repository.DefaultRetryOpts, user)
+
+	// There is a conflict - the login is already exists in the database
+	if errors.Is(err, repository.ErrConflict) {
+		return ErrLoginTaken
+	}
+
+	// There is another error
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *UserService) Login(ctx context.Context, user model.User) error {
+	// Ger user from repository
+	userInRepo, err := s.repository.Get(ctx, repository.DefaultRetryOpts, user.Login)
+	if err != nil {
+		return err
+	}
+
+	// TODO: password hashing
+
+	// If user doesn`t exist or password is wrong
+	if (userInRepo == model.User{}) || !(user.Password == userInRepo.Password) {
+		return ErrLoginWrong
+	}
+
 	return nil
 }
